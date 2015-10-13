a54c3532a659f76d31c776d88c9d492f95821550
Antonio Quijano <antonioquijano@Antonios-MacBook-Pro.local>
2015-10-13 10:14:12 -0600

    Commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('dburry', ['ngRoute', 'ui.materialize']);
+var app = angular.module('dburry', ['ngRoute', 'ui.materialize', 'uiGmapgoogle-maps', 'ngMaterial']);
 
 app.config(function ($routeProvider){
   $routeProvider
@@ -13,13 +13,15 @@ app.config(function ($routeProvider){
     templateUrl: 'js/view/pricing.html'
   })
   .when('/contact', {
-    templateUrl: 'js/view/contact.html'
+    templateUrl: 'js/view/contact.html',
+    controller: 'homeCtrl'
   })
   .when('/appointment', {
     templateUrl: 'js/view/appointment.html'
   })
   .when('/media', {
-    templateUrl: 'js/view/media.html'
+    templateUrl: 'js/view/media.html',
+    controller: 'flickr'
   })
   .otherwise('/home')
 })
